6e602137e21901ccf878ccf2c71721527f953e19
Hemyani <hdg2512@gmail.com>
2022-11-23 21:58:21 +0530

    Add project files.

---
+++ Emp Wage Computation Problem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Employee_Wage_Computation_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1.EmpAttendance();
+            //Console.ReadLine();
+
+            //UC2.EmpWage();    
+            //Console.ReadLine();
+
+            //UC3.AddPartTime();
+            //Console.ReadLine();
+
+            //UC4.EmpWages();
+            //Console.ReadLine();
+
+            //UC5.EmpWages();
+            //Console.ReadLine();
+
+            //UC6.DaysOrHours();
+            //Console.ReadLine();
+        }
+    }
+}
