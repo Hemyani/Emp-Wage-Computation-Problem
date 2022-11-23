ca25c8df53c1d6d49bc0cae84be6f2f5153c3a3e
Hemyani <hdg2512@gmail.com>
2022-11-23 22:01:51 +0530

    Check Employee is Present or Absent

--- Emp Wage Computation Problem/Program.cs
+++ Emp Wage Computation Problem/Program.cs
@@ -1,27 +1,12 @@
 ﻿using System;
-namespace Employee_Wage_Computation_Problem
+namespace Emp_Wage_Computation_Problem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //UC1.EmpAttendance();
-            //Console.ReadLine();
-
-            //UC2.EmpWage();    
-            //Console.ReadLine();
-
-            //UC3.AddPartTime();
-            //Console.ReadLine();
-
-            //UC4.EmpWages();
-            //Console.ReadLine();
-
-            //UC5.EmpWages();
-            //Console.ReadLine();
-
-            //UC6.DaysOrHours();
-            //Console.ReadLine();
+            UC1.EmpAttendance();
+            Console.ReadLine();
         }
     }
 }
